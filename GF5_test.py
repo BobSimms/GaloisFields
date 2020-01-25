a42fe3e416295f2e6258d92481ba415d4316e3e5
Bobsimms <BobSimms@users.noreply.github.com>
2020-01-24 23:38:14 -0500

    Update GF5_test.py

--- GF5_test.py
+++ GF5_test.py
@@ -13,6 +13,7 @@ from IntegerFunctions import *
 import codecs
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
+# confirm we can show any Greek letter as a variable
 print('\u03b1\u03b2\u03b3\u03b4\u03b5\u03b6\u03b7\u03b8')
 print('\u03b9\u03ba\u03bb\u03bc\u03bd\u03be\u03bf\u03c1')
 print('\u03c2\u03c3\u03c4\u03c5\u03c6\u03c7\u03c8\u03c9')
